3e71fccc71f4170191b361edaccb3bd2e2673d8a
Marc Pichler <marcpi@edu.aau.at>
2019-07-26 00:14:53 +0200

    Add missing prefixes according to SI.

--- SimplePrefix/PrefixContainers/SI.cs
+++ SimplePrefix/PrefixContainers/SI.cs
@@ -8,21 +8,64 @@ namespace SimplePrefix.PrefixContainers
     /// </summary>
     public class SI : PrefixContainer
     {
-        [PrefixField] public readonly Prefix Yocto = new DecimalPrefix(-24);
-        [PrefixField] public readonly Prefix Zepto = new DecimalPrefix(-21);
-        [PrefixField] public readonly Prefix Atto = new DecimalPrefix(-18);
-        [PrefixField] public readonly Prefix Femto = new DecimalPrefix(-15);
-        [PrefixField] public readonly Prefix Pico = new DecimalPrefix(-12);
-        [PrefixField] public readonly Prefix Nano = new DecimalPrefix( -9);
-        [PrefixField] public readonly Prefix Micro = new DecimalPrefix(-6);
-        [PrefixField] public readonly Prefix Milli = new DecimalPrefix(-3);
-        [PrefixField] public readonly Prefix Kilo = new DecimalPrefix(3);
-        [PrefixField] public readonly Prefix Mega = new DecimalPrefix(6);
-        [PrefixField] public readonly Prefix Giga = new DecimalPrefix(9);
-        [PrefixField] public readonly Prefix Tera = new DecimalPrefix(12);
-        [PrefixField] public readonly Prefix Peta = new DecimalPrefix(15);
-        [PrefixField] public readonly Prefix Exa = new DecimalPrefix(18);
-        [PrefixField] public readonly Prefix Zetta = new DecimalPrefix(21);
-        [PrefixField] public readonly Prefix Yotta = new DecimalPrefix(24);
+        [PrefixField("yocto")] [PrefixField("y")]
+        public readonly Prefix Yocto = new DecimalPrefix(-24);
+
+        [PrefixField("zepto")] [PrefixField("z")]
+        public readonly Prefix Zepto = new DecimalPrefix(-21);
+
+        [PrefixField("atto")] [PrefixField("a")]
+        public readonly Prefix Atto = new DecimalPrefix(-18);
+
+        [PrefixField("femto")] [PrefixField("f")]
+        public readonly Prefix Femto = new DecimalPrefix(-15);
+
+        [PrefixField("pico")] [PrefixField("p")]
+        public readonly Prefix Pico = new DecimalPrefix(-12);
+
+        [PrefixField("nano")] [PrefixField("n")]
+        public readonly Prefix Nano = new DecimalPrefix(-9);
+
+        [PrefixField("micro")] [PrefixField("µ")]
+        public readonly Prefix Micro = new DecimalPrefix(-6);
+
+        [PrefixField("milli")] [PrefixField("m")]
+        public readonly Prefix Milli = new DecimalPrefix(-3);
+        
+        [PrefixField("centi")] [PrefixField("c")]
+        public readonly Prefix Centi = new DecimalPrefix(-2);
+
+        [PrefixField("deci")] [PrefixField("d")]
+        public readonly Prefix Deci = new DecimalPrefix(-1);
+
+        [PrefixField("deca")] [PrefixField("da")]
+        public readonly Prefix Deca = new DecimalPrefix(1);
+
+        [PrefixField("hecto")] [PrefixField("h")]
+        public readonly Prefix Hecto = new DecimalPrefix(2);
+
+        [PrefixField("kilo")] [PrefixField("k")]
+        public readonly Prefix Kilo = new DecimalPrefix(3);
+
+        [PrefixField("mega")] [PrefixField("M")]
+        public readonly Prefix Mega = new DecimalPrefix(6);
+
+        [PrefixField("giga")] [PrefixField("G")]
+        public readonly Prefix Giga = new DecimalPrefix(9);
+
+        [PrefixField("tera")] [PrefixField("T")]
+        public readonly Prefix Tera = new DecimalPrefix(12);
+
+        [PrefixField("peta")] [PrefixField("P")]
+        public readonly Prefix Peta = new DecimalPrefix(15);
+
+        [PrefixField("exa")] [PrefixField("E")]
+        public readonly Prefix Exa = new DecimalPrefix(18);
+
+        [PrefixField("zetta")] [PrefixField("Z")]
+        public readonly Prefix Zetta = new DecimalPrefix(21);
+
+        [PrefixField("yotta")] [PrefixField("Y")]
+        public readonly Prefix Yotta = new DecimalPrefix(24);
     }
 }
--- SimplePrefixTests/PrefixContainersTests/SiPrefixTests.cs
+++ SimplePrefixTests/PrefixContainersTests/SiPrefixTests.cs
@@ -6,13 +6,18 @@ namespace SimplePrefixTests.PrefixContainersTests
 {
     public class SiPrefixTests
     {
-        private Dictionary<string, decimal> _expectedValues;
         private SI _prefixes;
 
         [SetUp]
         public void SetUp()
         {
-            _expectedValues = new Dictionary<string, decimal>
+            _prefixes = new SI();
+        }
+        
+        [Test]
+        public void FieldNameAndValueAsExpected()
+        {
+            var expectedValues = new Dictionary<string, decimal>
             {
                 {"Yocto", 0.000000000000000000000001m},
                 {"Zepto", 0.000000000000000000001m},
@@ -22,6 +27,10 @@ namespace SimplePrefixTests.PrefixContainersTests
                 {"Nano",  0.000000001m},
                 {"Micro", 0.000001m},
                 {"Milli", 0.001m},
+                {"Centi", 0.01m},
+                {"Deci",  0.1m},
+                {"Deca",  10m},
+                {"Hecto", 100m},
                 {"Kilo",  1000m},
                 {"Mega",  1000000m},
                 {"Giga",  1000000000m},
@@ -32,13 +41,73 @@ namespace SimplePrefixTests.PrefixContainersTests
                 {"Yotta", 1000000000000000000000000m}
             };
 
-            _prefixes = new SI();
+            foreach (var (key, value) in expectedValues)
+            {
+                Assert.AreEqual(value, (decimal) _prefixes[key]);
+            }
         }
         
         [Test]
-        public void ValuesAreAsExpected()
+        public void FieldAlternateNameAndValueAsExpected()
         {
-            foreach (var (key, value) in _expectedValues)
+            var expectedValues = new Dictionary<string, decimal>
+            {
+                {"yocto", 0.000000000000000000000001m},
+                {"zepto", 0.000000000000000000001m},
+                {"atto",  0.000000000000000001m},
+                {"femto", 0.000000000000001m},
+                {"pico",  0.000000000001m},
+                {"nano",  0.000000001m},
+                {"micro", 0.000001m},
+                {"milli", 0.001m},
+                {"centi", 0.01m},
+                {"deci",  0.1m},
+                {"deca",  10m},
+                {"hecto", 100m},
+                {"kilo",  1000m},
+                {"mega",  1000000m},
+                {"giga",  1000000000m},
+                {"tera",  1000000000000m},
+                {"peta",  1000000000000000m},
+                {"exa",   1000000000000000000m},
+                {"zetta", 1000000000000000000000m},
+                {"yotta", 1000000000000000000000000m}
+            };
+
+            foreach (var (key, value) in expectedValues)
+            {
+                Assert.AreEqual(value, (decimal) _prefixes[key]);
+            }
+        }
+        
+        [Test]
+        public void FieldSymbolNamesAndValuesAsExpected()
+        {
+            var expectedValues = new Dictionary<string, decimal>
+            {
+                {"y", 0.000000000000000000000001m},
+                {"z", 0.000000000000000000001m},
+                {"a", 0.000000000000000001m},
+                {"f", 0.000000000000001m},
+                {"p", 0.000000000001m},
+                {"n", 0.000000001m},
+                {"µ", 0.000001m},
+                {"m", 0.001m},
+                {"c", 0.01m},
+                {"d", 0.1m},
+                {"da",10m},
+                {"h", 100m},
+                {"k", 1000m},
+                {"M", 1000000m},
+                {"G", 1000000000m},
+                {"T", 1000000000000m},
+                {"P", 1000000000000000m},
+                {"E", 1000000000000000000m},
+                {"Z", 1000000000000000000000m},
+                {"Y", 1000000000000000000000000m}
+            };
+
+            foreach (var (key, value) in expectedValues)
             {
                 Assert.AreEqual(value, (decimal) _prefixes[key]);
             }
